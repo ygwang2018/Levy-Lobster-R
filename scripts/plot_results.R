094020c7395ee8fd767785b5313e6d1b2fd45169
You-Gan Wang <46190900+ygwang2018@users.noreply.github.com>
2025-12-04 21:03:40 +1000

    Create plot_results.R

---
+++ scripts/plot_results.R
@@ -0,0 +1,12 @@
+# Plot orchestrator
+# This script sources all figure scripts in scripts/plots/
+
+source("scripts/plots/fig2.R")
+source("scripts/plots/fig3.R")
+source("scripts/plots/fig4.R")
+source("scripts/plots/fig5.R")
+source("scripts/plots/fig6.R")
+source("scripts/plots/fig7.R")
+source("scripts/plots/fig8.R")
+source("scripts/plots/fig9.R")
+source("scripts/plots/fig10.R")
