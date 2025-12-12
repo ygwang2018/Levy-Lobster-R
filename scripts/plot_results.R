3441eb2b78cbeb27f915d2249717b1f15683708e
You-Gan Wang <46190900+ygwang2018@users.noreply.github.com>
2025-12-12 12:05:09 +1000

    Update plot_results.R

--- scripts/plot_results.R
+++ scripts/plot_results.R
@@ -2,9 +2,6 @@
 # This script sources all figure scripts in scripts/plots/
 
 source("scripts/plots/fig1.R")
-source("scripts/plots/fig2.R")
 source("scripts/plots/fig3.R")
 source("scripts/plots/fig4.R")
 source("scripts/plots/fig5.R")
-source("scripts/plots/fig6.R")
-source("scripts/plots/fig7.R")
