26feb4eda25055356547a9511949ffc30c785f34
You-Gan Wang <46190900+ygwang2018@users.noreply.github.com>
2025-12-13 12:41:48 +1000

    Update table2_igf.R

--- scripts/tables/table2_igf.R
+++ scripts/tables/table2_igf.R
@@ -1,19 +1,19 @@
 test_table2_igf <- function() {
     
+
     dat <- lobster
     
     log_dinvgauss <- function(x, mu, lambda) {
-        0.5 * (log(lambda) - log(2*pi) - 3*log(x)) -
+        0.5 * (log(lambda) - log(2 * pi) - 3 * log(x)) -
             lambda * (x - mu)^2 / (2 * mu^2 * x)
     }
     
-    LL_IG <- function(par, L, I, T) {
+    LL_IG_fixed <- function(par, Linf, L, I, T) {
         
-        Linf   <- par[1]
-        lambda <- par[2]
-        k      <- par[3]
+        lambda <- par[1]
+        k      <- par[2]
         
-        if (Linf <= max(L) + 1e-6 || lambda <= 0 || k <= 0)
+        if (lambda <= 0 || k <= 0)
             return(1e10)
         
         mu <- (Linf - L) * (1 - exp(-k * T))
@@ -26,26 +26,27 @@ test_table2_igf <- function() {
         return(-sum(ll))
     }
     
-    fit_IGF <- function(df, start, lower, upper) {
+    fit_IGF_fixed <- function(df, Linf, start, lower, upper) {
         
-        T <- df$INT / 365.25
+        T <- df$NINT / 365.25
         I <- df$INC
-        L <- df$PL
+        L <- df$CL
         
         res <- optim(
-            par     = start,
-            fn      = LL_IG,
-            L       = L,
-            I       = I,
-            T       = T,
-            method  = "L-BFGS-B",
-            lower   = lower,
-            upper   = upper,
+            par    = start,           # (lambda, k)
+            fn     = LL_IG_fixed,
+            Linf   = Linf,
+            L      = L,
+            I      = I,
+            T      = T,
+            method = "L-BFGS-B",
+            lower  = lower,
+            upper  = upper,
             control = list(maxit = 2000)
         )
         
         logLik <- -res$value
-        p      <- 3
+        p      <- 2                # FIXED Linf
         AIC    <- 2 * p - 2 * logLik
         
         list(
@@ -55,27 +56,33 @@ test_table2_igf <- function() {
         )
     }
     
-    ## Female
-    fit_f <- fit_IGF(
-        df    = dat[dat$SEX == 1, ],
-        start = c(180, 1, 0.1),
-        lower = c(100, 0.01, 0.01),
-        upper = c(300, 10, 0.4)
+    df_f <- dat[dat$SEX == 1, ]
+    Linf_f <- max(df_f$CL) + 10   # explicit fixed choice
+    
+    fit_f <- fit_IGF_fixed(
+        df    = df_f,
+        Linf  = Linf_f,
+        start = c(1.0, 0.05),
+        lower = c(0.01, 0.01),
+        upper = c(10.0, 0.5)
     )
     
-    ## Male
-    fit_m <- fit_IGF(
-        df    = dat[dat$SEX == 2, ],
-        start = c(160, 1, 0.1),
-        lower = c(160, 0.01, 0.01),
-        upper = c(180, 10, 1)
+    df_m <- dat[dat$SEX == 2, ]
+    Linf_m <- max(df_m$CL) + 50   # explicit fixed choice
+    
+    fit_m <- fit_IGF_fixed(
+        df    = df_m,
+        Linf  = Linf_m,
+        start = c(1.0, 0.05),
+        lower = c(0.01, 0.01),
+        upper = c(10.0, 1.0)
     )
     
     table2_results <- data.frame(
         Sex    = c("Female", "Male"),
-        Linf   = c(fit_f$par[1], fit_m$par[1]),
-        lambda = c(fit_f$par[2], fit_m$par[2]),
-        k      = c(fit_f$par[3], fit_m$par[3]),
+        Linf   = c(Linf_f, Linf_m),      # FIXED values
+        lambda = c(fit_f$par[1], fit_m$par[1]),
+        k      = c(fit_f$par[2], fit_m$par[2]),
         logLik = c(fit_f$logLik, fit_m$logLik),
         AIC    = c(fit_f$AIC, fit_m$AIC)
     )
@@ -85,10 +92,11 @@ test_table2_igf <- function() {
     
     write.csv(
         table2_results,
-        "results/tables/table2_igf.csv",
+        "results/tables/table2_igf_fixedLinf.csv",
         row.names = FALSE
     )
     
     return(table2_results)
 }
+
 test_table2_igf()
